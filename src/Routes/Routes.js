f3e75591ca0ec5264efa73deab4a9327d02a0598
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-25 19:01:02 +0600

    Basic Navbar SetUp

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
 import Category from "../pages/Category/Category"
+import Courses from "../pages/Courses/Courses";
 
 export const routes = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ export const routes = createBrowserRouter([
             {
                 path: '/category/:id',
                 element: <Category></Category>
+            },
+            {
+                path: '/courses/:id',
+                element: <Courses></Courses>
             }
         ]
     }
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import LeftSideNav from '../LeftSideNav.js/LeftSideNav';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <Navbar collapseOnSelect className='mb-5' expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">Coder E-Learning</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Courses</Nav.Link>
+                        <Nav.Link href="#features">Blog</Nav.Link>
+                        <Nav.Link href="#pricing">F.A.Q.</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                    <div className='d-lg-none'>
+                        <LeftSideNav></LeftSideNav>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
