e53336a7979241184b03608f4d1dbb566f5646a8
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-27 23:48:38 +0600

    Some Fixe

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -6,17 +6,17 @@ const FAQ = () => {
         <div>
             <h3>FAQ Section</h3>
             <div className='blogf'>
-                <h1 className='blog-headerf'>Frequntly Asked Questions</h1>
+                <h3 className='blog-headerf'>Frequntly Asked Questions</h3>
                 <div className='blog-sectionf'>
-                    <h2>What type of Code Will I Found?</h2>
+                    <h2>What type of Course Will I Found?</h2>
                     <p>You will Found Only Computer Programming Related Course Here</p>
                 </div>
                 <div className='blog-sectionf'>
-                    <h2>How can I Pay for the Course?</h2>
+                    <h3>How can I Pay for the Course?</h3>
                     <p>You Can Pay With VISA or MasterCard</p>
                 </div>
                 <div className='blog-sectionf'>
-                    <h2>Can We get Discount?</h2>
+                    <h3>Can We get Discount?</h3>
                     <p>Yes, Keep Your Eyes on Our Website</p>
                 </div>
             </div>
